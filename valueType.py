91cd10efa92f4585090b1edfd57ae5e9bb109110
Xezal Berçem <77122915+bercemxzl@users.noreply.github.com>
2021-01-07 22:36:40 +0300

    Add files via upload

---
+++ valueType.py
@@ -0,0 +1,42 @@
+
+print("Hello Xezal")
+
+#python yapısı süslü parantez kabul etmez bunu yerine tab boşluğu kabul eder.
+if(6<5):
+	print("bunu yaz")
+
+print("bunu yazma")
+
+if 5>2:
+	print("Doğrudur")
+
+#tab boşluğu gereksiz yere kullanılmamalı
+
+x = 5
+
+y = "Xezal"
+
+z = 4.34
+
+print(type(x))
+
+print(type(z))
+
+print(type(y))
+
+#komut satırı
+
+"""
+3 tırnak işareti kullanırsan geniş bir comut satırı alanına sahip olursun 
+
+bu alanın içerisine yazılan herhangi bir kod işlem görmeyecektir.
+"""
+degisken1=4.34
+
+stringDegisken="X"
+
+castDegisken= int (degisken1)
+
+print(castDegisken)
+
+
